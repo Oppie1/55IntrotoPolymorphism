ea1100e58903a94b3d2ced6a58ad2325952a0abf
Oppie1 <atindall729@gmail.com>
2025-03-26 17:00:01 -0500

    Update main.cpp
    Updated with notes

--- 55IntrotoPolymorphism/main.cpp
+++ 55IntrotoPolymorphism/main.cpp
@@ -30,10 +30,10 @@ public:
 	int main() {
 		Ninja n;
 		Monster m;
-		Enemy* enemy1 = &n;
-		Enemy* enemy2 = &m;
-		enemy1->setAttackPower(29);
-		enemy2->setAttackPower(99);
-		n.attack();
-		m.attack();
+		Enemy* enemy1 = &n;		// becuase ninja is of type enemy, this is valid
+		Enemy* enemy2 = &m;		//anything that an enemy can do, monster can do
+		enemy1->setAttackPower(29); // ninja is just a more specific type of enemy
+		enemy2->setAttackPower(99); // envery enemy has a setAttackPower
+		n.attack();					// Enemy class does not have attack
+		m.attack();					//virtual members make this even easier
 	}
